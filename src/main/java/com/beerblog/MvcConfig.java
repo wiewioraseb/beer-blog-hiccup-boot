2e0b48f031aec8d166a884e6795bdfea5b2ebc70
SW <wiewiora.seb@gmail.com>
2016-06-08 22:31:39 +0200

    Added application.properties and MvcConfig.java

---
+++ src/main/java/com/beerblog/MvcConfig.java
@@ -0,0 +1,18 @@
+package main.java.com.beerblog;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("index-main");
+        registry.addViewController("/welcome").setViewName("welcome");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
